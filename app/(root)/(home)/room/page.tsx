1d9db74bda87679da901c3ac5d0ecdad45eca87d
manishyoudumb <darksalterego99@gmail.com>
2024-07-11 22:47:36 +0530

    chore: Update Home and related pages with new layout and styles

--- app/(root)/(home)/room/page.tsx
+++ app/(root)/(home)/room/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
     <h1 className='text-3xl font-bold'>
-      Previous Meets
+      Personal Room
     </h1>
   </section>
   )
