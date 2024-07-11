881f2009697661de8e54828856d46c920523e628
manishyoudumb <darksalterego99@gmail.com>
2024-07-11 22:01:26 +0530

    chore: Update Sidebar component with dynamic sidebar links

---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/home.svg',
+    },
+
+    {
+        label: 'Upcoming Meets',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },
+
+    {
+        label: 'Previous Meetings',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg',
+    },
+
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/video-record.svg',
+    },
+
+    {
+        label: 'Personal Room',
+        route: '/room',
+        imgUrl: '/icons/personl-room.svg',
+    },
+]
