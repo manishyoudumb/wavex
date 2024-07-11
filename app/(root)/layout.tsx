58b90906d355058153e92429b6da11bb3ade0a94
manishyoudumb <darksalterego99@gmail.com>
2024-07-11 20:42:39 +0530

    chore: Update layout components and styles

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,9 +3,9 @@ import React, { Children, ReactNode } from 'react'
 const Rootlayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-        Navbar
+        
         {children}
-        Footer
+        
     </main>
   )
 }
