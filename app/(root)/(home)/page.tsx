917147ecd879a34d1a3e3424ba1289ea48afc3a1
manishyoudumb <darksalterego99@gmail.com>
2024-07-12 15:19:34 +0530

    Update Home page with upcoming meeting information

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,9 +3,15 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>
-        Home
-      </h1>
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism max-w-[200px] rounded py-2 text-center text-base font-normal'>
+
+            Upcoming Meeting at: 12:30pm 
+          </h2>
+
+        </div>
+      </div>
     </section>
   )
 }
